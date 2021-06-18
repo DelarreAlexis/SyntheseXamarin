4193e444337a1f3cf95bc8cd53d78619a16edd7f
Alexis Delarre <adelarre@amo-technologies.com>
2021-06-18 15:26:27 +0200

    Ajout du squelette de l'exercice de synthese XAMARIN

---
+++ ExerciceSynthese/Dal/TacheDal.cs
@@ -0,0 +1,35 @@
+﻿using ExerciceSynthese.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciceSynthese.Dal
+{
+	public class TacheDal
+	{
+		private SQLiteConnection _dbb;
+		public TacheDal()
+		{
+			_dbb = App.SQLiteConnection;
+		}
+		
+		public List<Tache> SelectAll()
+		{
+			return _dbb.Table<Tache>().ToList();
+		}
+
+		public Tache Select(int id)
+		{
+			return _dbb.Table<Tache>().Where(x => x.Id == id).FirstOrDefault();
+		}
+
+		public void Sauvegarder(Tache tache)
+		{
+			if (tache.Id != 0)
+				_dbb.Update(tache);
+			else
+				_dbb.Insert(tache);
+		}
+	}
+}
---
+++ ExerciceSynthese/Services/DataBaseGetter.cs
@@ -0,0 +1,21 @@
+﻿using ExerciceSynthese.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ExerciceSynthese.Services
+{
+	public class DataBaseGetter
+	{
+		public SQLiteConnection Get()
+		{
+			string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "GestionTache.db3");
+			var db = new SQLiteConnection(dbPath);
+			db.CreateTable<Tache>();
+			db.CreateTable<Profil>();
+			return db;
+		}
+	}
+}
